1bcc613b4150585d837502d952d9e32200ff6798
seedevk8s <seedevk8s@gmail.com>
2021-12-28 22:32:31 +0900

    ioc 적용전 일반적인 제어--개발자가 객체 생성해서 실행

---
+++ src/kr/co/ezenac/ioc/Encoder.java
@@ -0,0 +1,11 @@
+package kr.co.ezenac.ioc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Encoder {
+
+    public String encode(String message) {
+        return Base64.getEncoder().encodeToString(message.getBytes());
+    }
+}
