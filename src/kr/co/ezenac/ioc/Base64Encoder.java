89238d0f6be67c0cdb4bd2f85d7b7461faafe598
seedevk8s <seedevk8s@gmail.com>
2021-12-28 22:48:04 +0900

    클래스 이름 리펙토링 및 인터페이스 상속 구조로 변경함

--- src/kr/co/ezenac/ioc/Base64Encoder.java
+++ src/kr/co/ezenac/ioc/Base64Encoder.java
@@ -1,9 +1,8 @@
 package kr.co.ezenac.ioc;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-public class Encoder {
+public class Base64Encoder implements IEncoder {
 
     public String encode(String message) {
         return Base64.getEncoder().encodeToString(message.getBytes());
--- src/kr/co/ezenac/ioc/Main.java
+++ src/kr/co/ezenac/ioc/Main.java
@@ -6,12 +6,12 @@ public class Main {
         String url = "www.google.com/books/it?page=10&size=20&name=springframework";
 
         // Base 64 encoding
-        Encoder encoder = new Encoder();
+        IEncoder encoder = new Base64Encoder();
         String result = encoder.encode(url);
         System.out.println(result);
 
         // url encoding
-        UrlEncoder urlEncoder = new UrlEncoder();
+        IEncoder urlEncoder = new UrlEncoder();
         String urlResult = urlEncoder.encode(url);
         System.out.println(urlResult);
     }
